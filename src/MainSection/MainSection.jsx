33f2f50318596c824d38626cd75900b535d66fb2
Asal Yousefi <asalyousefi.dev@gmail.com>
2026-04-27 19:42:22 +0330

    hero section completed

---
+++ src/MainSection/MainSection.jsx
@@ -0,0 +1,9 @@
+import About from "./About";
+
+export default function MainSection() {
+    return (
+        <main>
+            <About />
+        </main>
+    )
+}
--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header/Header";
 import "./main.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import MainSection from "./MainSection/MainSection";
 
 function App() {
   return (
     <>
       <Header />
+      <MainSection />
     </>
   );
 }
