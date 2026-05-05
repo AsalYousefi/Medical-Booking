e409a75384fb0e2bf76141307320cabcb9110ed6
Asal Yousefi <asalyousefi.dev@gmail.com>
2026-05-05 12:38:50 +0330

    Timeline section created

--- src/MainSection/MainSection.jsx
+++ src/MainSection/MainSection.jsx
@@ -1,11 +1,15 @@
 import About from "./About/About";
+import Gallery from "./Gallery/Gallery";
 import Home from "./Home/Home";
+import Timeline from "./Timeline/Timeline";
 
 export default function MainSection() {
     return (
         <main>
             <Home />
             <About />
+            <Gallery />
+            <Timeline />
         </main>
     )
 }
---
+++ src/MainSection/Timeline/Timeline.jsx
@@ -0,0 +1,9 @@
+export default function Timeline() {
+    return (
+        <section id="timeline" className="timeline">
+            <div className="container">
+                <h2 className="fw-bold text-black text-center">Our Timeline</h2>
+            </div>
+        </section>
+    )
+}
