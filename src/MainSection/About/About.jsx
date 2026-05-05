de6666f7ebe4105f468281c54f891d45168e7444
Asal Yousefi <asalyousefi.dev@gmail.com>
2026-05-05 12:39:08 +0330

    About section completed

--- src/MainSection/About/About.jsx
+++ src/MainSection/About/About.jsx
@@ -1,15 +1,32 @@
 export default function About() {
-    return (
-        <section className="about my-5" id="about">
-            <div className="container">
-                <div className="d-md-flex justify-content-between">
-                    <div>
-                        <h2 className="fw-bold text-black">Meet Dr. Carson</h2>
-                    </div>
-                    <div></div>
-                </div>
-                <div></div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="about my-5" id="about">
+      <div className="container">
+        <div className="d-md-flex justify-content-between align-items-center">
+          <div className="about-texts">
+            <h2 className="about-title my-4 fw-bold text-black">
+              Meet Dr. Carson
+            </h2>
+            <p className="fw-light about-description">
+              Protect yourself and others by wearing masks and washing hands
+              frequently. Outdoor is safer than indoor for gatherings or holding
+              events. People who get sick with Coronavirus disease (COVID-19)
+              will experience mild to moderate symptoms and recover without
+              special treatments.
+            </p>
+            <p className="fw-light about-description">
+              You can feel free to use this CSS template for your medical
+              profession or health care related websites. You can support us a
+              little via PayPal if this template is good and useful for your
+              work.
+            </p>
+          </div>
+          <div className="experience mx-auto text-center d-flex justify-content-center align-items-center rounded-circle bg-white shadow-lg">
+            <span className="fw-bold w-75"><span className="experience-number text-primary me-3">12</span>Years of Experience</span>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
 }
