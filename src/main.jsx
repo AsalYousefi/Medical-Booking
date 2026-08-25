a65cb8dc535dadcf84ebfbd257cd6f01ca2a6553
Asal Yousefi <asalyousefi.dev@gmail.com>
2026-08-25 15:03:33 +0330

    solve problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./App";
 import { StrictMode } from "react";
 import "./i18n"
 import "bootstrap/dist/css/bootstrap.css";
