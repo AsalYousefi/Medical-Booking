623aaff55c02122d0e2e31fbc55b062d426d2c3c
Asal Yousefi <asalyousefi.dev@gmail.com>
2026-09-15 18:56:02 +0330

    fix position of header logo

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,17 +8,13 @@ type HeaderProps = {
   activeSection: string;
 };
 
-export default function Header(props: HeaderProps) {
+export default function Header({ navOptions, activeSection }: HeaderProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   const { t } = useTranslation();
 
   return (
-    // <header className="d-grid position-sticky top-0 start-0 w-100 z-3 p-3 py-lg-4 shadow-lg bg-light">
     <header className="d-flex justify-content-around align-items-center position-sticky top-0 start-0 w-100 z-3 p-3 py-lg-4 shadow-lg bg-light">
-      <div className="d-none d-lg-block">
-        <LngBtn />
-      </div>
       <div
         className={`d-lg-none pointer menu-btn d-flex flex-column justify-content-between ${
           isOpen && "open"
@@ -30,50 +26,49 @@ export default function Header(props: HeaderProps) {
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
-      <nav className="horizontal-nav">
-        <ul className="m-0 list-unstyled d-lg-flex justify-content-lg-around align-items-lg-center">
-          {props.navOptions.map((navOp, i) => {
-            if (i < 3)
-              return (
-                <li
-                  key={i}
-                  className={`${
-                    props.activeSection === navOp && "active"
-                  } d-none d-lg-block text-capitalize nav-item fw-bold`}
-                >
-                  <a href={`#${navOp}`} className="nav-link">
-                    {t(`header.nav.${navOp}`)}
-                  </a>
-                </li>
-              );
-          })}
-          <li className="text-center">
-            <h1 className="logo text-primary text-capitalize fw-bold">
-              <a href="/">
-                {t("header.logo.h1")}
-                <span className="d-block">{t("header.logo.p")}</span>
+      <nav className="d-none d-lg-flex justify-content-around align-items-center w-100">
+        <LngBtn />
+        <ul className="m-0 list-unstyled d-flex justify-content-between align-items-center w-50">
+          {navOptions.slice(0, 3).map((navOp, i) => (
+            <li
+              key={i}
+              className={`${
+                activeSection === navOp && "active"
+              } text-capitalize nav-item fw-bold`}
+            >
+              <a href={`#${navOp}`} className="nav-link">
+                {t(`header.nav.${navOp}`)}
               </a>
-            </h1>
-          </li>
-          {props.navOptions.map((navOp, i) => {
-            if (i >= 3)
-              return (
-                <li
-                  key={i}
-                  className={`${
-                    props.activeSection === navOp && "active"
-                  } d-none d-lg-block text-capitalize nav-item fw-bold`}
-                >
-                  <a href={`#${navOp}`} className="nav-link">
-                    {t(`header.nav.${navOp}`)}
-                  </a>
-                </li>
-              );
-          })}
+            </li>
+          ))}
         </ul>
       </nav>
 
-      <AuthBtn />
+      <h1 className="logo text-center text-primary text-capitalize fw-bold">
+        <a href="/">
+          {t("header.logo.h1")}
+          <span className="d-block">{t("header.logo.p")}</span>
+        </a>
+      </h1>
+
+      <nav className="d-none d-lg-flex justify-content-around align-items-center w-100">
+        <ul className="m-0 list-unstyled d-flex justify-content-between align-items-center w-50">
+          {navOptions.slice(3).map((navOp, i) => (
+            <li
+              key={i}
+              className={`${
+                activeSection === navOp && "active"
+              } text-capitalize nav-item fw-bold`}
+            >
+              <a href={`#${navOp}`} className="nav-link">
+                {t(`header.nav.${navOp}`)}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <AuthBtn />
+      </nav>
+      <div className="d-lg-none"><AuthBtn /></div>
 
       {isOpen && (
         <div className="d-lg-none position-absolute z-3 w-100 bg-light start-0 top-100">
@@ -83,12 +78,12 @@ export default function Header(props: HeaderProps) {
                 <li className="nav-item py-1 my-1">
                   <LngBtn />
                 </li>
-                {props.navOptions.map((navOp, i) => {
+                {navOptions.map((navOp, i) => {
                   return (
                     <li
                       key={i}
                       className={`${
-                        props.activeSection === navOp && "active"
+                        activeSection === navOp && "active"
                       } text-capitalize nav-item fw-500 py-1 my-1`}
                     >
                       <a href={`#${navOp}`} className="nav-link">
